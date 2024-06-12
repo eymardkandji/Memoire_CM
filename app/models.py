5b5fb144e4587ee3f82dfe9bfc41ad9209c276ed
eymardkandji <eymardkandji@gmail.com>
2024-06-12 20:28:47 +0100

    Level 0.2

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+
+""""
+class Connexion(models.Model):
+    host = models.CharField(max_length=255)
+    userid = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    ssl = models.BooleanField(default=False)
+"""
--- app/views.py
+++ app/views.py
@@ -1,12 +1,36 @@
 from django.shortcuts import render
 
+from proxmoxer import ProxmoxAPI as pveAPI
+
 
 # Create your views here.
 def base(request):
-    nodes = {
-        "nodes": ['Node 1', 'Node 2', 'Node 3']
-    }
-    return render(request, "web/base.html", context=nodes)
+
+    host = "192.168.168.100"
+    userid = "root@pam"
+    password = "node1"
+    ssl = False
+    pve = pveAPI(
+        host,
+        user=userid,
+        password=password,
+        verify_ssl=ssl
+    )
+    nodes_name = []
+    for _ in pve.cluster.status.get():
+        if _['type'] == 'node':
+            nodes_name.append(_['name'])
+        else:
+            cluster_name = _['name']
+    return render(request, "web/base.html", context={
+        "nodes_name": nodes_name,
+        "cluster_name": cluster_name
+    })
+
+
+def index(request):
+    return render(request, "web/index.html")
+
 
 def netwok(request):
     return render(request, "web/network.html")
